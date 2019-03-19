b7c8b0d577c299e8bd240541873b58fbd3fcfc06
Seamie-irl <seamus.l.leonard@gmail.com>
2019-03-19 15:36:23 +0000

    Updated Q8

---
+++ Exercise8.py
@@ -0,0 +1,9 @@
+# 1st March 2019
+#Q8. Write a program that outputs today’s date and time in the format ”Monday, January
+# 10th 2019 at 1:15pm”.
+
+# logic: get the month and day names then just concatenate the whole lot
+
+# from previous exercises, bolt on DateTime and Calendar
+import datetime 
+import calendar 
