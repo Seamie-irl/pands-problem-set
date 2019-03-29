40b0e5d2e199024cadcc4f9f7fb945ad1238c28d
Seamie-irl <seamus.l.leonard@gmail.com>
2019-03-29 20:27:19 +0000

    not sure why but status says it was edited

--- datetimeQ.py
+++ datetimeQ.py
@@ -7,23 +7,24 @@
 # referenced: 
 # https://stackoverflow.com/a/20007730 for the Ordinal function **
 # https://stackoverflow.com/a/13855243 
-# and https://www.pythoncentral.io/how-to-display-the-date-and-time-using-python/ for the 12-hour time ***
+# and https://www.pythoncentral.io/how-to-display-the-date-and-time-using-python/ 
+# for the 12-hour time ***
 
 # from previous exercises, bolt on DateTime and Calendar
 import datetime as d
-import calendar
-import math
-import time
+import calendar as c
+import math as m
+import time as t
 
 rightNow=d.datetime.now() #get the timestamp
-monthName=calendar.month_name[rightNow.month] # get the name for the current month
-dayName=calendar.day_name[rightNow.day] # get the name of today
-sTime=time.strftime("%I:%M %p") # ***       get the time in the 12hr clock
-ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(math.floor(n/10)%10!=1)*(n%10<4)*n%10::4]) # **
+monthName=c.month_name[rightNow.month] # get the name for the current month
+dayName=rightNow.strftime("%A") # get the name of today
+sTime=t.strftime("%I:%M %p") # ***       get the time in the 12hr clock
+ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(m.floor(n/10)%10!=1)*(n%10<4)*n%10::4]) # **
 # the above line is taken from the aforementioned reference
 
 # now put it all together and print
-sResult=dayName + ", " + monthName + " " + ordinal(13) +" " + str(rightNow.year) + " at " + sTime
+sResult=dayName + ", " + monthName + " " + ordinal(rightNow.day) +" " + str(rightNow.year) + " at " + sTime
 print(sResult)
 
 # note: it would be possible to put most of the calculations / conversions into the print statement
